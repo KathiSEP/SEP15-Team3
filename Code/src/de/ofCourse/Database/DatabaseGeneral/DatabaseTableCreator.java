c233bbfdc6b6afd31f2b166a8da14f3c768dec3e
blackydeg <sebastian@nrschwarz.de>
2015-06-04 18:04:57 +0200

    Update Test

--- Code/src/de/ofCourse/Database/DatabaseGeneral/DatabaseTableCreator.java
+++ Code/src/de/ofCourse/Database/DatabaseGeneral/DatabaseTableCreator.java
@@ -27,7 +27,7 @@ public class DatabaseTableCreator {
 	
 	private static final String CREATE_FORM_OF_ADDRESS =
 			"CREATE TYPE form_of_address AS ENUM (" +
-					"'MR.', 'MS.'" +
+					"'MR', 'MS'" +
     		")";
     
     private static final String CREATE_ROLE =
@@ -65,7 +65,7 @@ public class DatabaseTableCreator {
 				"admin_verification BOOLEAN NOT NULL," +
 				"profile_image BYTEA," +
 				"role ROLE NOT NULL," +
-				"status STATUS NOT NULL" +
+				"status STATUS NOT NULL,veri_string VARCHAR(130) UNIQUE NOT NULL" +
 			");" +
 			"ALTER SEQUENCE users_id_seq RESTART WITH 10000";
     
