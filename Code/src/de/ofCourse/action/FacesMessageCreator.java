4bc24f3c9eda2edd2162d0983fafaf56a1b23ac8
KathiSEP <katharina_hoelzl@web.de>
2015-06-08 21:29:18 +0200

    faces message creator update

--- Code/src/de/ofCourse/action/FacesMessageCreator.java
+++ Code/src/de/ofCourse/action/FacesMessageCreator.java
@@ -1,8 +1,15 @@
 package de.ofCourse.action;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
 import javax.faces.context.FacesContext;
 
+/**
+ * Creates faces messages.
+ * 
+ * @author Tobias Fuchs, Kathi Hölzl
+ *
+ */
 public class FacesMessageCreator {
     /**
      * Creates a new FacesMessage with the given message.
@@ -12,13 +19,8 @@ public class FacesMessageCreator {
      * @param message
      *            message to display
      */
-    public static void createFacesMessageWithMessage(String component, String message) {
-
-	FacesContext facesContext = FacesContext.getCurrentInstance();
-	FacesMessage msg = new FacesMessage(message);
-	msg.setSeverity(FacesMessage.SEVERITY_INFO);
-	facesContext.addMessage(component, msg);
-	facesContext.renderResponse();
+    public static void createFacesMessage(String component, String message) {
+        createFacesMessage(component, message, FacesMessage.SEVERITY_INFO);
     }
 
     /**
@@ -29,13 +31,43 @@ public class FacesMessageCreator {
      * @param e
      *            exception which was thrown
      */
-    public static void createFacesMessageWithException(String component, Exception e) {
+    public static void createFacesMessage(String component, Exception e) {
+        createFacesMessage(component, e.getMessage(),
+                FacesMessage.SEVERITY_INFO);
+    }
 
-	FacesContext facesContext = FacesContext.getCurrentInstance();
-	FacesMessage msg = new FacesMessage(e.getMessage());
-	msg.setSeverity(FacesMessage.SEVERITY_INFO);
-	facesContext.addMessage(component, msg);
-	facesContext.renderResponse();
+    /**
+     * Creates the FacesMessage with the given message of the exception and the
+     * given severity.
+     * 
+     * @param component
+     *            binding of the message to the component
+     * @param e
+     *            exception which was thrown
+     * @param facesSeverity
+     *            severity of the message
+     */
+    public static void createFacesMessage(String component, Exception e,
+            Severity facesSeverity) {
+        createFacesMessage(component, e.getMessage(), facesSeverity);
+    }
 
+    /**
+     * Creates the FacesMessage with the given message and the given severity.
+     * 
+     * @param component
+     *            binding of the message to the component
+     * @param message
+     *            text of the message
+     * @param facesSeverity
+     *            severity of the message
+     */
+    public static void createFacesMessage(String component, String message,
+            Severity facesSeverity) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        FacesMessage msg = new FacesMessage(message);
+        msg.setSeverity(FacesMessage.SEVERITY_INFO);
+        facesContext.addMessage(component, msg);
+        facesContext.renderResponse();
     }
 }
