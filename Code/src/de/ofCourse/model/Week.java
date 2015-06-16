79871946b789e5063a053a138189a0670a8b89b9
cretu90 <cretu@fim.uni-passau.de>
2015-06-17 01:32:12 +0200

    minor updates

---
+++ Code/src/de/ofCourse/model/Week.java
@@ -0,0 +1,18 @@
+package de.ofCourse.model;
+
+public class Week {
+
+	private String Monday;
+	
+	private String Tuesday;
+	
+	private String Wednesay;
+	
+	private String Thursday;
+	
+	private String Friday;
+	
+	private String Saturday;
+	
+	private String Sunday;
+}
