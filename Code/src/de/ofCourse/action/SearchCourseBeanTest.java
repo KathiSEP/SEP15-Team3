ac90fdb560efb124353951b782cabf9f29cd0342
cretu90 <cretu@fim.uni-passau.de>
2015-06-19 02:52:48 +0200

    -

--- Code/src/de/ofCourse/action/SearchCourseBeanTest.java
+++ Code/src/de/ofCourse/action/SearchCourseBeanTest.java
@@ -1,9 +1,19 @@
 package de.ofCourse.action;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import java.util.HashMap;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+
+import de.ofCourse.system.Connection;
 
 public class SearchCourseBeanTest {
 	
@@ -16,8 +26,24 @@ public class SearchCourseBeanTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSearch() {
+		PowerMockito.mockStatic(FacesContext.class);
+		FacesContext fc = mock(FacesContext.class);
+		Mockito.when(FacesContext.getCurrentInstance()).thenReturn(fc);
+
+		// Mock the ExternalContext
+		ExternalContext ec = mock(ExternalContext.class);
+		Mockito.when(fc.getExternalContext()).thenReturn(ec);
+
+		// Determine returning of getRequestParameterMap()
+		pm = new HashMap<String, String>();
+		Mockito.when(ec.getRequestParameterMap()).thenReturn(pm);
+
+		// Mock the connection
+		PowerMockito.mockStatic(Connection.class);
+		conn = mock(Connection.class);
+		Mockito.when(Connection.create()).thenReturn(conn);
+		
 	}
 
 }
