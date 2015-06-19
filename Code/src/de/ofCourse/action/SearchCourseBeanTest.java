b92dbb7d6a8a52554f3949c83029979e19dffe7a
cretu90 <cretu@fim.uni-passau.de>
2015-06-19 03:29:30 +0200

    Update test

--- Code/src/de/ofCourse/action/SearchCourseBeanTest.java
+++ Code/src/de/ofCourse/action/SearchCourseBeanTest.java
@@ -3,7 +3,9 @@ package de.ofCourse.action;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
+import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
@@ -13,11 +15,18 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 
+import de.ofCourse.Database.dao.CourseDAO;
+import de.ofCourse.model.CourseUnit;
 import de.ofCourse.system.Connection;
 
 public class SearchCourseBeanTest {
 	
+	// RequestparameterMap
+    private Map<String, String> pm;
+	
 	private SearchCourseBean bean;
+	
+	private Connection conn;
 	
 	@SuppressWarnings("deprecation")
     @Before
@@ -43,6 +52,14 @@ public class SearchCourseBeanTest {
 		PowerMockito.mockStatic(Connection.class);
 		conn = mock(Connection.class);
 		Mockito.when(Connection.create()).thenReturn(conn);
+		
+		// Mock CourseDAO
+		PowerMockito.mockStatic(CourseDAO.class);
+		
+		// Create courseUnit
+		CourseUnit cu1 = new CourseUnit();
+		cu1.setStartime(new GregorianCalendar(2015, 6, 1).getTime());
+		cu1.setEndtime(new GregorianCalendar(2015, 6, 2).getTime());
 		
 	}
 
