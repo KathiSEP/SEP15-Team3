776b04742f697433efa5c0a74d265940f8e27a9b
KathiSEP <katharina_hoelzl@web.de>
2015-06-25 10:54:33 +0200

    CourseInstructorValidator und language

--- Code/src/de/ofCourse/customValidator/CourseInstructorValidator.java
+++ Code/src/de/ofCourse/customValidator/CourseInstructorValidator.java
@@ -3,6 +3,8 @@
  */
 package de.ofCourse.customValidator;
 
+import java.util.Map;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.component.UIComponent;
@@ -10,13 +12,15 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import javax.servlet.http.HttpSession;
 
 import de.ofCourse.action.SessionUserBean;
 import de.ofCourse.databaseDAO.CourseDAO;
-import de.ofCourse.databaseDAO.UserDAO;
 import de.ofCourse.exception.InvalidDBTransferException;
+import de.ofCourse.model.Language;
 import de.ofCourse.system.Connection;
 import de.ofCourse.system.Transaction;
+import de.ofCourse.utilities.LanguageManager;
 
 /**
  * Checks whether the entered name of the course instructor exists in the
@@ -39,6 +43,22 @@ public class CourseInstructorValidator implements Validator {
     @Override
     public void validate(FacesContext fc, UIComponent component, Object value)
             throws ValidatorException {
+        
+        Map<String, Object> sessionMap = FacesContext
+                .getCurrentInstance().getExternalContext().getSessionMap();
+        
+        Language lang = null;
+        
+        if(sessionMap.containsKey("lang")) {
+            lang = Language.fromString(sessionMap.get("lang").toString());
+        } else {
+            lang = Language.DE;
+            HttpSession session = (HttpSession) FacesContext
+                    .getCurrentInstance()
+                    .getExternalContext()
+                    .getSession(true);
+            session.setAttribute("lang", lang.toString());
+        }
 
         String courseLeaderIDString = value.toString();
         int courseLeaderID = 0;
@@ -46,13 +66,22 @@ public class CourseInstructorValidator implements Validator {
         try {
             courseLeaderID = Integer.parseInt(courseLeaderIDString);
         } catch (NumberFormatException ex) {
-            throw new ValidatorException(new FacesMessage("courseLeaderID",
-                    "Eingegebene Kursleiter-ID ist keine Zahl!"));
+            throw new ValidatorException(
+                    new FacesMessage("courseLeaderID",
+                                    LanguageManager.getInstance().
+                                    getProperty(
+                                         "createCourse.Validator."
+                                         + "CourseInstruktorNoNumber", lang)));
         }
 
         if (courseLeaderID < 1) {
-            throw new ValidatorException(new FacesMessage("courseLeaderID",
-                    "Eingegebene Kursleiter-ID muss eine positive Zahl sein."));
+            throw new ValidatorException(
+                    new FacesMessage("courseLeaderID",
+                                    LanguageManager.getInstance().
+                                    getProperty(
+                                          "createCourse.Validator."
+                                          + "CourseInstruktorNoPositivNumber", 
+                                          lang)));
         }
 
         Transaction transaction = new Connection();
@@ -60,13 +89,20 @@ public class CourseInstructorValidator implements Validator {
         try {
             if (!CourseDAO.courseLeaderExists(transaction, courseLeaderID)) {
                 throw new ValidatorException(new FacesMessage("courseLeaderID",
-                        "Eingegebene Kursleiter-ID existiert nicht im System."));
+                        LanguageManager.getInstance().
+                        getProperty(
+                              "createCourse.Validator."
+                              + "CourseInstruktorNotExisting", 
+                              lang)));
             }
             transaction.commit();
         } catch (InvalidDBTransferException e) {
             transaction.rollback();
             throw new ValidatorException(new FacesMessage("courseLeaderID",
-                    "Ein interner Datenbankfehler trat auf."));
+                    LanguageManager.getInstance().
+                    getProperty(
+                          "createCourse.Validator.CourseInstruktorDatabase", 
+                          lang)));
         }
 
     }
