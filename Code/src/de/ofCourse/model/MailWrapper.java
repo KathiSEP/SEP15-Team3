46ff168110dfacc6cc2f49db766b3d5b4860efb2
FuchsSEP <fuchs249@live.de>
2015-06-26 03:00:38 +0200

    MailWrpper

--- Code/src/de/ofCourse/model/MailWrapper.java
+++ Code/src/de/ofCourse/model/MailWrapper.java
@@ -1,5 +1,50 @@
 package de.ofCourse.model;
 
 public class MailWrapper {
+    
+    private Integer unitId;
+    
+    
+    public MailWrapper(Integer id, String email){
+	unitId = id;
+	mail = email;
+	
+    }
+    
+    
+    
+    /**
+     * @return the unitId
+     */
+    public Integer getUnitId() {
+        return unitId;
+    }
+
+    /**
+     * @param unitId the unitId to set
+     */
+    public void setUnitId(Integer unitId) {
+        this.unitId = unitId;
+    }
+
+    /**
+     * @return the mail
+     */
+    public String getMail() {
+        return mail;
+    }
+
+    /**
+     * @param mail the mail to set
+     */
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    private String mail;
+    
+    
+    
+    
 
 }
