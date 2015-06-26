51d0890736623b677aa0f71ef852d0689d14d3de
cretu90 <cretu@fim.uni-passau.de>
2015-06-26 06:05:11 +0200

    Kommentare

--- Code/src/de/ofCourse/model/Week.java
+++ Code/src/de/ofCourse/model/Week.java
@@ -1,83 +1,196 @@
+/**
+ * This package represents all the models which are used
+ */
 package de.ofCourse.model;
 
+/**
+ * Contains each week day's content
+ * 
+ * @author Patrick Cretu
+ *
+ */
 public class Week {
 
+	/**
+	 * The hour of the tuples start time
+	 */
 	private String hour;
 	
+	/**
+	 * The week's Monday content
+	 */
 	private String monday;
 	
+	/**
+	 * The week's Tuesday content
+	 */
 	private String tuesday;
 	
+	/**
+	 * The week's Wednesday content
+	 */
 	private String wednesday;
 	
+	/**
+	 * The week's Thursday content
+	 */
 	private String thursday;
 	
+	/**
+	 * The week's Friday content
+	 */
 	private String friday;
 	
+	/**
+	 * The week's Saturday content
+	 */
 	private String saturday;
 	
+	/**
+	 * The week's Sunday content
+	 */
 	private String sunday;
 
+	/**
+	 * 
+	 * @return the tuple's start time
+	 */
 	public String getHour() {
 		return hour;
 	}
 
+	/**
+	 * Sets the week's start time
+	 * 
+	 * @param hour
+	 *           the start time
+	 */
 	public void setHour(String hour) {
 		this.hour = hour;
 	}
 
+	/**
+	 * 
+	 * @return the week's Monday content
+	 */
 	public String getMonday() {
 		return monday;
 	}
 
+	/**
+	 * Sets the week's Monday content
+	 * 
+	 * @param monday
+	 *             the Monday content
+	 */
 	public void setMonday(String monday) {
 		this.monday = monday;
 	}
 
+	/**
+	 * 
+	 * @return the week's Tuesday content
+	 */
 	public String getTuesday() {
 		return tuesday;
 	}
 
+	/**
+	 * Sets the week's Tuesday content
+	 * 
+	 * @param tuesday
+	 *              the Tuesday content
+	 */
 	public void setTuesday(String tuesday) {
 		this.tuesday = tuesday;
 	}
 
+	/**
+	 * 
+	 * @return the week's Wednesday content
+	 */
 	public String getWednesday() {
 		return wednesday;
 	}
 
+	/**
+	 * Sets the week's Wednesday content
+	 * 
+	 * @param wednesday
+	 *                the Wednesday content
+	 */
 	public void setWednesday(String wednesday) {
 		this.wednesday = wednesday;
 	}
 
+	/**
+	 * 
+	 * @return the week's Thursday content
+	 */
 	public String getThursday() {
 		return thursday;
 	}
 
+	/**
+	 * Sets the week's Thursday content
+	 * 
+	 * @param thursday
+	 *               the Thursday content
+	 */
 	public void setThursday(String thursday) {
 		this.thursday = thursday;
 	}
 
+	/**
+	 * 
+	 * @return the week's Friday content
+	 */
 	public String getFriday() {
 		return friday;
 	}
 
+	/**
+	 * Sets the week's Friday content
+	 * 
+	 * @param friday
+	 *             the Friday content
+	 */
 	public void setFriday(String friday) {
 		this.friday = friday;
 	}
 
+	/**
+	 * 
+	 * @return the week's Saturday content
+	 */
 	public String getSaturday() {
 		return saturday;
 	}
 
+	/**
+	 * Sets the week's Saturday content
+	 * 
+	 * @param saturday
+	 *               the Saturday content
+	 */
 	public void setSaturday(String saturday) {
 		this.saturday = saturday;
 	}
 
+	/**
+	 * 
+	 * @return the week's Sunday content
+	 */
 	public String getSunday() {
 		return sunday;
 	}
 
+	/**
+	 * Sets the week's Sunday content
+	 * 
+	 * @param sunday
+	 *             the Sunday content
+	 */
 	public void setSunday(String sunday) {
 		this.sunday = sunday;
 	}
--- Code/src/de/ofCourse/model/WeekDay.java
+++ Code/src/de/ofCourse/model/WeekDay.java
@@ -1,15 +1,56 @@
+/**
+ * * This package represents all the models which are used
+ */
 package de.ofCourse.model;
 
+/**
+ * represents the different days within a calendar week
+ * 
+ * @author Patrick Cretu
+ *
+ */
 public enum WeekDay {
 
+	/**
+	 * The week's Monday
+	 */
 	MONDAY,
+	
+	/**
+	 * The week's Monday
+	 */
 	TUESDAY,
+	
+	/**
+	 * The week's Monday
+	 */
 	WEDNESDAY,
+	
+	/**
+	 * The week's Monday
+	 */
 	THURSDAY,
+	
+	/**
+	 * The week's Monday
+	 */
 	FRIDAY,
+	
+	/**
+	 * The week's Monday
+	 */
 	SATURDAY,
+	
+	/**
+	 * The week's Monday
+	 */
 	SUNDAY;
 	
+	/**
+	 * Returns an array containing all week days.
+	 * 
+	 * @return an array containing all week days
+	 */
 	public static WeekDay[] getWeekDays() {
 		return WeekDay.values();
 	}
