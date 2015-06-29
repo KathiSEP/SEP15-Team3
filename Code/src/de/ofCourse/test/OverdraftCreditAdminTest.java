84299964c780bf090a27b4badd7860aa3ae0c3a2
FuchsSEP <fuchs249@live.de>
2015-06-29 20:04:54 +0200

    test

---
+++ Code/src/de/ofCourse/test/OverdraftCreditAdminTest.java
@@ -0,0 +1,83 @@
+package de.ofCourse.test;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class OverdraftCreditAdminTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8003/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testOverdraftCreditAdmin() throws Exception {
+    driver.get(baseUrl + "OfCourse/facelets/open/index.xhtml");
+    driver.findElement(By.id("generalNavigationForm:authenticateLink")).click();
+    driver.findElement(By.id("formLogin:usernameLogin")).clear();
+    driver.findElement(By.id("formLogin:usernameLogin")).sendKeys("admin1");
+    driver.findElement(By.id("formLogin:passwordLogin")).clear();
+    driver.findElement(By.id("formLogin:passwordLogin")).sendKeys("Password!123");
+    driver.findElement(By.id("formLogin:login")).click();
+    driver.findElement(By.linkText("Administration")).click();
+    driver.findElement(By.linkText("Seitenverwaltung")).click();
+    driver.findElement(By.id("formGiveCredit:amountGivenCredit")).clear();
+    driver.findElement(By.id("formGiveCredit:amountGivenCredit")).sendKeys("EUR zehn");
+    driver.findElement(By.id("formGiveCredit:giveCredit")).click();
+    driver.findElement(By.id("formGiveCredit:amountGivenCredit")).clear();
+    driver.findElement(By.id("formGiveCredit:amountGivenCredit")).sendKeys("EUR 10,00");
+    driver.findElement(By.id("formGiveCredit:giveCredit")).click();
+    driver.findElement(By.id("generalNavigationForm:logoutLink")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
