4b3868b7638b1631860e93a331490e0d6b1da73d
Ricky <ricky.strohmeier@gmx.de>
2015-07-02 20:25:31 +0200

    Tests

---
+++ Code/src/de/ofCourse/test/SignUpForYogaCourseunitsTest.java
@@ -0,0 +1,83 @@
+package de.ofCourse.test;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SignUpForYogaCourseunitsTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://muhingen.ddns.net/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testSignUpForYogaCourseunits() throws Exception {
+    driver.get(baseUrl + "OfCourse/facelets/open/index.xhtml");
+    driver.findElement(By.id("generalNavigationForm:authenticateLink")).click();
+    driver.findElement(By.id("formLogin:usernameLogin")).clear();
+    driver.findElement(By.id("formLogin:usernameLogin")).sendKeys("Kathi5");
+    driver.findElement(By.id("formLogin:passwordLogin")).clear();
+    driver.findElement(By.id("formLogin:passwordLogin")).sendKeys("Muhmuh1!");
+    driver.findElement(By.id("formLogin:login")).click();
+    driver.findElement(By.id("myCoursesList:courseTable:0:details")).click();
+    driver.findElement(By.id("j_idt113:j_idt115:0:anmelden")).click();
+    driver.findElement(By.id("j_idt113:j_idt115:1:anmelden")).click();
+    driver.findElement(By.id("j_idt113:j_idt115:2:anmelden")).click();
+    driver.findElement(By.id("j_idt113:j_idt115:3:anmelden")).click();
+    driver.findElement(By.id("j_idt113:j_idt115:4:anmelden")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
