b4b94aa49edaf8eefeb377608cc26bc92d486656
KathiSEP <katharina_hoelzl@web.de>
2015-07-02 20:56:18 +0200

    Update lostpassword test

---
+++ Code/src/de/ofCourse/test/LostPasswordTest.java
@@ -0,0 +1,79 @@
+package de.ofCourse.test;
+
+/**
+ * Testing of lost password. This test is geared to the test T40-110 from 
+ * our product brief. 
+ * 
+ * @author Katharina Hölzl
+ */
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LostPasswordTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8003/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testBlabla() throws Exception {
+    driver.get(baseUrl + "OfCourse/facelets/open/index.xhtml");
+    driver.findElement(By.id("generalNavigationForm:authenticateLink")).click();
+    driver.findElement(By.id("formForgotPassword:passwordEMail")).clear();
+    driver.findElement(By.id("formForgotPassword:passwordEMail")).sendKeys("katharina_hoelzl@web.de");
+    driver.findElement(By.id("formForgotPassword:newPassword")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
