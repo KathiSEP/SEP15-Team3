ddc7c6a437a90dadcda5520903a40b1eeb470423
KathiSEP <katharina_hoelzl@web.de>
2015-07-02 21:24:11 +0200

    Update Verbesserungen

--- Code/src/de/ofCourse/test/ChangeUserRoleTest.java
+++ Code/src/de/ofCourse/test/ChangeUserRoleTest.java
@@ -1,19 +1,15 @@
 package de.ofCourse.test;
 
-/**
- * Testing of lost password. This test is geared to the test T40-110 from 
- * our product brief. 
- * 
- * @author Katharina Hölzl
- */
-
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class LostPasswordTest {
+public class ChangeUserRoleTest {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -27,12 +23,27 @@ public class LostPasswordTest {
   }
 
   @Test
-  public void testBlabla() throws Exception {
+  public void testChangeUserRole() throws Exception {
     driver.get(baseUrl + "OfCourse/facelets/open/index.xhtml");
     driver.findElement(By.id("generalNavigationForm:authenticateLink")).click();
-    driver.findElement(By.id("formForgotPassword:passwordEMail")).clear();
-    driver.findElement(By.id("formForgotPassword:passwordEMail")).sendKeys("katharina.hoelzl93@gmx.de");
-    driver.findElement(By.id("formForgotPassword:newPassword")).click();
+    driver.findElement(By.id("formLogin:usernameLogin")).clear();
+    driver.findElement(By.id("formLogin:usernameLogin")).sendKeys("admin1");
+    driver.findElement(By.id("formLogin:passwordLogin")).clear();
+    driver.findElement(By.id("formLogin:passwordLogin")).sendKeys("Password!123");
+    driver.findElement(By.id("formLogin:login")).click();
+    driver.findElement(By.linkText("Administration")).click();
+    driver.findElement(By.linkText("Seitenverwaltung")).click();
+    driver.findElement(By.id("userManagement:manageUsers")).click();
+    
+    new Select(driver.findElement(By.id("formFilterCourses:filterSearchParam"))).selectByVisibleText("Nutzername");
+    driver.findElement(By.id("formFilterCourses:filterInput")).clear();
+    driver.findElement(By.id("formFilterCourses:filterInput")).sendKeys("Kathi5");
+    driver.findElement(By.id("formFilterCourses:searchUser")).click();
+    driver.findElement(By.id("formResultTable:users:0:details")).click();
+    driver.findElement(By.id("formSettings:changeSettings")).click();
+    new Select(driver.findElement(By.id("formChangeSettings:userRoleSelection"))).selectByVisibleText("Kursleiter");
+    driver.findElement(By.id("formChangeSettings:saveSettings")).click();
+    driver.findElement(By.id("generalNavigationForm:logoutLink")).click();
   }
 
   @After
