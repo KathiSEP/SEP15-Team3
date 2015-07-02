f447b577889797e28bc7b48548f4600e731e9585
Ricky <ricky.strohmeier@gmx.de>
2015-07-03 00:18:48 +0200

    Tests

--- Code/src/de/ofCourse/test/SignUpForCourseAndUnitsTest.java
+++ Code/src/de/ofCourse/test/SignUpForCourseAndUnitsTest.java
@@ -24,7 +24,7 @@ public class SignUpForCourseAndUnitsTest {
   @Before
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    baseUrl = "http://muhingen.ddns.net/";
+    baseUrl = "http://localhost:8003/OfCourse/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
