92b21717c7e3baceb41abc7d15102effde76c253
FuchsSEP <fuchs249@live.de>
2015-07-08 12:25:06 +0200

    Noch ein BugFix

--- Code/src/de/ofCourse/system/AccessPhaseListener.java
+++ Code/src/de/ofCourse/system/AccessPhaseListener.java
@@ -350,26 +350,44 @@ public class AccessPhaseListener implements PhaseListener {
 	    default:
 		for (int i = 0; i < anonymousUsers.size() && !isPermitted; ++i) {
 		    if (fctx.getViewRoot().getViewId()
-			.equals(anonymousUsers.get(i))
-			|| isOwnProfile(fctx)) {
+			.equals(anonymousUsers.get(i))) {
 			isPermitted = true;
 		    }
 		}
 	}
 	}
+	
+	if(isPermitted){
+	    isPermitted = isOwnProfile(fctx, role);}
+	
 	return isPermitted;
     }
     
-    private boolean isOwnProfile(FacesContext fctx) {
-    	if (fctx.getViewRoot().getViewId().endsWith("profile.xhtml")) {
-	    	int userID = Integer.parseInt(FacesContext.getCurrentInstance()
+    
+    /**
+     * @param fctx
+     * @return
+     */
+    private boolean isOwnProfile(FacesContext fctx, String role){
+	boolean ownProfile = true;
+	if (fctx.getViewRoot().getViewId().endsWith("profile.xhtml") && !role.equals("SYSTEM_ADMINISTRATOR")) {
+	    	int userID  = 0;
+	    		try{
+	    		userID= Integer.parseInt(FacesContext.getCurrentInstance()
 	                .getExternalContext().getRequestParameterMap()
-	                .get("userID"));
-	    	if (sessionUser.getUserID() == userID) {
-	    		return true;
+	                .get("userID"));}
+	    	catch(NumberFormatException e){
+	    	    userID = sessionUser.getUserID();
 	    	}
-	    }
-    	return false;
+	    	if (sessionUser.getUserID() != userID) {
+	    		ownProfile = false;;
+	    	}
+	}
+	return ownProfile;
+	
+	
+	
+	
     }
 
     /**
@@ -423,6 +441,7 @@ public class AccessPhaseListener implements PhaseListener {
 	ArrayList<String> listForRegisteredUsers = new ArrayList<String>();
 
 	listForRegisteredUsers.add(URL_MY_COURSES);
+	listForRegisteredUsers.add(URL_PROFILE);
 	listForRegisteredUsers.add(URL_SCHEDULER);
 	listForRegisteredUsers.add(URL_PARTICIPANTS_LIST);
 	return listForRegisteredUsers;
@@ -452,7 +471,6 @@ public class AccessPhaseListener implements PhaseListener {
 	ArrayList<String> listForAdministrators = new ArrayList<String>();
 
 	listForAdministrators.add(URL_ADMIN_MANAGEMENT);
-	listForAdministrators.add(URL_PROFILE);
 	listForAdministrators.add(URL_SEARCH_USER);
 	listForAdministrators.add(URL_CREATE_USER);
 	listForAdministrators.add(URL_CREATE_COURSE);
