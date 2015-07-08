6b183da93a0f755ca7c57b0e24ae6028dde73e74
FuchsSEP <fuchs249@live.de>
2015-07-08 12:18:32 +0200

    Update BugFix

--- Code/src/de/ofCourse/system/AccessPhaseListener.java
+++ Code/src/de/ofCourse/system/AccessPhaseListener.java
@@ -350,39 +350,26 @@ public class AccessPhaseListener implements PhaseListener {
 	    default:
 		for (int i = 0; i < anonymousUsers.size() && !isPermitted; ++i) {
 		    if (fctx.getViewRoot().getViewId()
-			.equals(anonymousUsers.get(i))) {
+			.equals(anonymousUsers.get(i))
+			|| isOwnProfile(fctx)) {
 			isPermitted = true;
 		    }
 		}
 	}
 	}
-	
-	if(isPermitted){
-	    isPermitted = isOwnProfile(fctx, role);}
-	
 	return isPermitted;
     }
     
-    
-    /**
-     * @param fctx
-     * @return
-     */
-    private boolean isOwnProfile(FacesContext fctx, String role){
-	boolean ownProfile = true;
-	if (fctx.getViewRoot().getViewId().endsWith("profile.xhtml") && !role.equals("SYSTEM_ADMINISTRATOR")) {
+    private boolean isOwnProfile(FacesContext fctx) {
+    	if (fctx.getViewRoot().getViewId().endsWith("profile.xhtml")) {
 	    	int userID = Integer.parseInt(FacesContext.getCurrentInstance()
 	                .getExternalContext().getRequestParameterMap()
 	                .get("userID"));
-	    	if (sessionUser.getUserID() != userID) {
-	    		ownProfile = false;;
+	    	if (sessionUser.getUserID() == userID) {
+	    		return true;
 	    	}
-	}
-	return ownProfile;
-	
-	
-	
-	
+	    }
+    	return false;
     }
 
     /**
@@ -436,7 +423,6 @@ public class AccessPhaseListener implements PhaseListener {
 	ArrayList<String> listForRegisteredUsers = new ArrayList<String>();
 
 	listForRegisteredUsers.add(URL_MY_COURSES);
-	listForRegisteredUsers.add(URL_PROFILE);
 	listForRegisteredUsers.add(URL_SCHEDULER);
 	listForRegisteredUsers.add(URL_PARTICIPANTS_LIST);
 	return listForRegisteredUsers;
@@ -466,6 +452,7 @@ public class AccessPhaseListener implements PhaseListener {
 	ArrayList<String> listForAdministrators = new ArrayList<String>();
 
 	listForAdministrators.add(URL_ADMIN_MANAGEMENT);
+	listForAdministrators.add(URL_PROFILE);
 	listForAdministrators.add(URL_SEARCH_USER);
 	listForAdministrators.add(URL_CREATE_USER);
 	listForAdministrators.add(URL_CREATE_COURSE);
